858efb4be0b0ebd40e7d6505192d1cd7b4e9d6b9
andrzej.krzemien <andrzej.krzemien@antologic.com>
2022-04-14 12:37:05 +0200

    -task-1 Adding changes to a new branch

--- aem-guides-wknd/core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/SomeButtonModelImpl.java
+++ aem-guides-wknd/core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/SomeButtonModelImpl.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
 @Model(adaptables = SlingHttpServletRequest.class,
         adapters = {SomeButtonModel.class },
         resourceType = SomeButtonModelImpl.RESOURCE_TYPE,
