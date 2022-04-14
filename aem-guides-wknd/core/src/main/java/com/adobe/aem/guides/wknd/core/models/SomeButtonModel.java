8d52d6fd3efa36af13ceffbf8b0c0bf47998b438
andrzej.krzemien <andrzej.krzemien@antologic.com>
2022-04-14 10:24:22 +0200

    task-1- Added new branch with .gitignore and updated somebutton files

--- aem-guides-wknd/core/src/main/java/com/adobe/aem/guides/wknd/core/models/SomeButtonModel.java
+++ aem-guides-wknd/core/src/main/java/com/adobe/aem/guides/wknd/core/models/SomeButtonModel.java
@@ -1,10 +1,13 @@
 package com.adobe.aem.guides.wknd.core.models;
 
+import org.apache.sling.api.resource.Resource;
+
+import java.util.Iterator;
 import java.util.List;
 
 public interface SomeButtonModel {
 
     String getName();
 
-    List<String> getChildNodes();
+    Iterator<Resource> getChildNodes();
 }
